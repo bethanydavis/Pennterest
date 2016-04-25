0e93856ccab9e6bc260b17929e4539c6381b3c2b
bethanydavis <davisbet@seas.upenn.edu>
2016-04-25 02:39:53 -0400

    rubocop offenses

--- pennterest/db/schema.rb
+++ pennterest/db/schema.rb
@@ -13,38 +13,38 @@
 
 ActiveRecord::Schema.define(version: 20160424184441) do
 
-  create_table "boards", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'boards', force: :cascade do |t|
+    t.string   'title'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "pins", force: :cascade do |t|
-    t.string   "caption"
-    t.string   "category"
-    t.string   "source"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'pins', force: :cascade do |t|
+    t.string   'caption'
+    t.string   'category'
+    t.string   'source'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "about_me"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+  create_table 'users', force: :cascade do |t|
+    t.string   'name'
+    t.string   'about_me'
+    t.datetime 'created_at',                          null: false
+    t.datetime 'updated_at',                          null: false
+    t.string   'email',                  default: '', null: false
+    t.string   'encrypted_password',     default: '', null: false
+    t.string   'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.integer  'sign_in_count',          default: 0,  null: false
+    t.datetime 'current_sign_in_at'
+    t.datetime 'last_sign_in_at'
+    t.string   'current_sign_in_ip'
+    t.string   'last_sign_in_ip'
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index 'users', ['email'], name: 'index_users_on_email', unique: true
+  add_index 'users', ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
 
 end
