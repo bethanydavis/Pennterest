60c540a76df808b109202325d1440da0f1dd17f2
bethanydavis <davisbet@seas.upenn.edu>
2016-04-24 22:55:10 -0400

    typo

--- pennterest/app/models/board.rb
+++ pennterest/app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ActiveRecord::Base
   belongs_to :user
   has_many :boards_pins, dependent: :destroy
-  has_many :pinss, through: :boards_pins
+  has_many :pins, through: :boards_pins
 end
