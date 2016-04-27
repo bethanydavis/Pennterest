24beba21a79a594209970625ec954a52742c6ab6
bethanydavis <davisbet@seas.upenn.edu>
2016-04-27 01:16:24 -0400

    image uploading is cool now

--- pennterest/app/controllers/pins_controller.rb
+++ pennterest/app/controllers/pins_controller.rb
@@ -79,6 +79,6 @@ class PinsController < ApplicationController
   # Never trust parameters from the scary internet,
   # only allow the white list through.
   def pin_params
-    params.require(:pin).permit(:caption, :category, :source)
+    params.require(:pin).permit(:caption, :category, :source, :image)
   end
 end
--- pennterest/app/models/pin.rb
+++ pennterest/app/models/pin.rb
@@ -5,10 +5,10 @@ class Pin < ActiveRecord::Base
 
   has_attached_file :image, styles: {
     thumb: '100x100>',
-    square: '200x200#',
+    square: '150x150>',
     medium: '300x300>'
   }
 
   # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
