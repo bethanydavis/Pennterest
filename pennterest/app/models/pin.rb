aee5667dd657885720f61a169776bf87dd2e5988
bethanydavis <davisbet@seas.upenn.edu>
2016-04-26 23:45:03 -0400

    adding image functionality to pins

--- pennterest/app/models/pin.rb
+++ pennterest/app/models/pin.rb
@@ -3,5 +3,12 @@ class Pin < ActiveRecord::Base
   has_many :boards_pins, dependent: :destroy
   has_many :boards, through: :boards_pins
 
-  has_attached_file :image
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+  # Validate the attached image is image/jpg, image/png, etc
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
